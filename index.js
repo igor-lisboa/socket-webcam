50ae7a8ef2d1d493b9195e8af241f62480deb3e6
igor lisboa <igor.rodrigues.lisboa@gmail.com>
2019-08-22 13:26:41 -0300

    subindo alteracoes

--- index.js
+++ index.js
@@ -4,38 +4,45 @@ const http = require('http');
 const WebSocket = require('ws');
 const app = express();
 const httpServer = http.createServer(app);
+var localIpV4Address = require("local-ipv4-address");
 
-const PORT = process.env.PORT || 3000;
+localIpV4Address().then(function (ipAddress) {
+    console.log("Meu IP é: " + ipAddress);
 
-const wsServer = new WebSocket.Server({ server: httpServer }, () => console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
 
-// array of connected websocket clients
-let connectedClients = [];
+    const PORT = process.env.PORT || 3000;
 
-wsServer.on('connection', (ws, req) => {
-    console.log('Connected');
-    // add new connected client
-    connectedClients.push(ws);
-    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
-    ws.on('message', data => {
-        // send the base64 encoded frame to each connected ws
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
-            } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
-            }
+    const wsServer = new WebSocket.Server({ server: httpServer }, () => console.log(`WS server is listening at ws://${ipAddress}:${WS_PORT}`));
+
+    // array of connected websocket clients
+    let connectedClients = [];
+
+    wsServer.on('connection', (ws, req) => {
+        console.log('Connected');
+        // add new connected client
+        connectedClients.push(ws);
+        // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+        ws.on('message', data => {
+            // envia o frame em base64 para cada cliente conectado
+            connectedClients.forEach((ws, i) => {
+                if (ws.readyState === ws.OPEN) { // verifica se ainda esta conectado
+                    ws.send(data); // send
+                } else { // se nao esta conectado remove do array de ws conectados
+                    connectedClients.splice(i, 1);
+                }
+            });
         });
     });
-});
 
-// HTTP stuff
-app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')));
-app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './streamer.html')));
-app.get('/', (req, res) => {
-    res.send(`
+    // HTTP rotas
+    app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')));
+    app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './streamer.html')));
+    app.get('/', (req, res) => {
+        // response mostrado em tela
+        res.send(`
         <a href="streamer">Streamer</a><br>
         <a href="client">Client</a>
     `);
+    });
+    httpServer.listen(PORT, () => console.log(`HTTP server listening at http://${ipAddress}:${PORT}`));
 });
-httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # npm init -y && npm i -S ws express && touch index.js client.html streamer.html
 
+# npm install --save local-ipv4-address
+
 # add ("start": "node index.js") no package.json em scripts
 
 # npm start para iniciar a aplicação
